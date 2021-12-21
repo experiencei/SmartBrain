379e19b9deb6ccbb1b1e91197c2b274da8d54324
experiencei <ayelojahighbee01@gmail.com>
2021-12-22 00:20:52 +0100

    working on adding new features

--- src/App.js
+++ src/App.js
@@ -213,3 +213,16 @@ return reverseArr.join("")
 }
 
 reverseString2("my name is Ibrahim")
+
+
+function firstRecurLetter (input) {
+     for (let i = 0; i < input.length; i++) {
+       for (let j = i + 1; j < input.length; j++) {
+        if(input[i] === input[j]) {
+          return input[i]
+        }
+         
+       }
+       
+     }
+}
