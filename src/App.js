c63d8766af90f4933190865ec243b59501232c95
experiencei <ayelojahighbee01@gmail.com>
2021-12-21 22:37:09 +0100

    working on adding new features

--- src/App.js
+++ src/App.js
@@ -156,11 +156,40 @@ function checkBoolean (array1 , array2)  {
 
 function checkBoolean2 (array1 , array2) {
   for (let i = 0; i < array1.length; i++) {
-      for (let j = 0; j < array.length; j++) {
-        if (array[i]===array2[j]) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array1[i]===array2[j]) {
           return true
         }
       }
   }
   return false
 }
+
+
+function mergeSortedArray (array1 , array2) {
+  const mergedArray = [];
+  let array1Item = array1[0]
+  let array2Item = array2[0]
+  let i = 1;
+  let j = 1;
+
+
+  while (array1Item || array2Item){
+    if(array2Item === undefined || array1Item < array2Item){
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    }   
+    else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+   }
+   return mergedArray;
+
+}
+
+
+mergeSortedArray([1,2,5 ,6 ,8], [1,2,3,4,5,6,])
+
